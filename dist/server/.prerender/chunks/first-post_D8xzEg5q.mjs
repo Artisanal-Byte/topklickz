fa2698ac9e33a5026eaee5ef9f31fb79780b500d
RiyaPatel2002 <03112002riyapatel@gmail.com>
2026-08-26 18:19:41 +0530

    mnerging

---
+++ dist/server/.prerender/chunks/first-post_D8xzEg5q.mjs
@@ -0,0 +1,8 @@
+async function getMod() {
+						return import('./first-post_vh4ZzcjB.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
+
+export { defaultMod as default };
